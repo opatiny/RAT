c131bec501c546bad5232dc7c43a052d2c01423c
lpatiny <luc@patiny.com>
2017-04-30 08:19:25 +0200

    Fix softpwm (forgot initialisation)

--- arduino/Art/Action2.ino
+++ arduino/Art/Action2.ino
@@ -1,26 +1,23 @@
 #define THR_ACTION2 1
 
-
-NIL_WORKING_AREA(waThreadAction2, 100);
+NIL_WORKING_AREA(waThreadAction2, 200);
 NIL_THREAD(ThreadAction2, arg) {
-
 
   SoftPWMBegin();
   int action2Step = 0;
-
+  
   while (true) {
-
-    if (getParameter(PARAM_POWER) & 1<<BIT_ENABLE_PUMP) {
+    if (bitRead(getParameter(PARAM_POWER),BIT_ENABLE_PUMP)==1) {
       SoftPWMSet(IO1, getParameter(PARAM_PUMP));
+     // digitalWrite(IO1, HIGH);
     } else {
       SoftPWMSet(IO1, 0);
     }
-    
+
     nilThdSleepMilliseconds(40);
 
     action2Step++;
     setParameter(PARAM_ACTION2, action2Step);
-
   }
 }
 
